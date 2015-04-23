622637b4dff2924f774edeb81a80eb4c1a6e0447
David Ladowitz <david@ladowitz.com>
2015-04-23 15:29:52 -0700

    adding bower, bootrap and some routes

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-// access the express module
+//// modules
 var express = require("express");
+// var bodyParser = require("body-parser")
+
 
 // create an express server instance
 var app = express();
 
+// look in the view directory. Use index.html as default. Override app.get()
+app.use(express.static(__dirname + '/views'));
+
+// serve up static files
+app.use(express.static(__dirname + '/static'));
+
+// app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.json());
+
+// //logging middleware
+// app.use(function(req, res, next){
+//   console.log("Request at ", req.path);
+//   next();
+// })
+
+
+//// Routes
 // listen on at the root '/' of the domain
 app.get("/", function(request, response) {
-  response.send("<h1>Hello World!</h1>");
+  response.send("<h1>Hello There World!!!!</h1>");
  });
+
+app.post("/submit", function(request, response) {
+  console.log("post request coming in")
+  response.send("Thanks for the email address")
+ });
+
+
+
 
 // find the port from the environment
 var port = process.env["PORT"];
