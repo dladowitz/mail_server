7bef0418e928b2fa778c9b3760bb5d6ed67c5e14
David Ladowitz <david@ladowitz.com>
2015-05-02 22:43:54 -0700

    updating main.js

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -178,18 +178,18 @@ $(document).ready(function() {
 
 // preventDefault is stopping the form from being sent
 // Contact form
-  // var form = $('.form-horizontal');
-  // form.submit(function(event){
-  //   event.preventDefault();
-  //   var form_status = $('<div class="form_status"></div>');
-  //   $.ajax({
-  //     url: $(this).attr('action'),
-  //     beforeSend: function(){
-  //       form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
-  //     }
-  //   }).done(function(data){
-  //     form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
-  //   });
-  // });
+  var form = $('.form-horizontal');
+  form.submit(function(event){
+    event.preventDefault();
+    var form_status = $('<div class="form_status"></div>');
+    $.ajax({
+      url: ($(this).attr('action')),
+      beforeSend: function(){
+        form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+      }
+    }).done(function(data){
+      form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+    });
+  });
 
   
