dbaf57debef6b9d3addc3a7b5bb0911fe18dd668
David Ladowitz <david@ladowitz.com>
2015-05-08 13:26:34 -0700

    adding tests

--- test/basic_tests.js
+++ test/basic_tests.js
@@ -1,13 +1,13 @@
 var assert  = require("assert");
 var C       = require("../models/cash.js")
 
-describe('Array', function(){
-  describe('#indexOf', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+// describe('Array', function(){
+//   describe('#indexOf', function(){
+//     it('should return -1 when the value is not present', function(){
+//       assert.equal(-1, [1,2,3].indexOf(4));
+//     });
+//   });
+// });
 
 describe('Cash Register', function(){
   describe('Module C', function(){
@@ -16,9 +16,9 @@ describe('Cash Register', function(){
       assert.equal(typeof C.getChange, 'function');
     })
 
-    it('should return the correct change', function(){
-      assert.deepEqual(C.getChange(210,300), [50,20,20]);
-      assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
-    })
+    // it('should return the correct change', function(){
+    //   assert.deepEqual(C.getChange(210,300), [50,20,20]);
+    //   assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
+    // })
   })
 });
